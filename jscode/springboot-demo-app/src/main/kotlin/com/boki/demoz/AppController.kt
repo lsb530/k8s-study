b3955968add1901ad1d2905b282526f6e9ae6605
boki <lsb530@naver.com>
2025-06-02 11:40:44 +0900

    jscode) springboot env

--- jscode/springboot-demo-app/src/main/kotlin/com/boki/demoz/AppController.kt
+++ jscode/springboot-demo-app/src/main/kotlin/com/boki/demoz/AppController.kt
@@ -1,14 +1,26 @@
 package com.boki.demoz
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class AppController {
 
+    @Value("\${MY_ACCOUNT:default account}")
+    private lateinit var myAccount: String
+
+    @Value("\${MY_PASSWORD:default password}")
+    private lateinit var myPassword: String
+
     @GetMapping
     fun home(): String {
         return "Version 1.1"
+    }
+
+    @GetMapping("/info")
+    fun myInfo(): String {
+        return "myAccount: $myAccount, myPassword: $myPassword"
     }
 
 }
