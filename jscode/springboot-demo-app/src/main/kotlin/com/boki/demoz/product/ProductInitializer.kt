015762c89dd87179d3f0bbd85eb8592bfb058fce
boki <lsb530@naver.com>
2025-06-02 16:11:47 +0900

    jscode) mysql block external access(NodePort -> ClusterIP)

--- jscode/springboot-demo-app/src/main/kotlin/com/boki/demoz/product/ProductInitializer.kt
+++ jscode/springboot-demo-app/src/main/kotlin/com/boki/demoz/product/ProductInitializer.kt
@@ -13,12 +13,14 @@ open class ProductInitializer(
     @Transactional
     @EventListener(ApplicationReadyEvent::class)
     fun init(event: ApplicationReadyEvent) {
-        val product1 = Product(name = "포켓몬 피규어", price = 8000)
-        val product2 = Product(name = "편의점 과자", price = 1500)
-        val product3 = Product(name = "치킨 콜라 셋트", price = 23000)
-        productRepository.saveAll(
-            listOf(product1, product2, product3)
-        )
+        if (productRepository.count() == 0L) {
+            val product1 = Product(name = "포켓몬 피규어", price = 8000)
+            val product2 = Product(name = "편의점 과자", price = 1500)
+            val product3 = Product(name = "치킨 콜라 셋트", price = 23000)
+            productRepository.saveAll(
+                listOf(product1, product2, product3)
+            )
+        }
     }
 
 }
